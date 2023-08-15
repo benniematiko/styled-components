80c1787112ce983a47daae6d950acf377f2fc687
Ben Matiko <sidneymatiko@gmail.com>
2023-08-15 17:10:06 +0300

    add styling on MENU Bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyles } from "./components/styles/Globals.styled";
+import { ThemeProvider } from "styled-components";
+import "./App.css";
 
 function App() {
+  const theme = {
+    colors: {
+      white: "rgba(240, 247, 255, 1)",
+    },
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles />
+
+      <h1> Komotobo School</h1>
     </div>
   );
 }
---
+++ src/layout/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <div>Komotobo Mission</div>
+        <div>MENU</div>
+        <div>NavList</div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
